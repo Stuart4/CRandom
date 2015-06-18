d4a076c146e4b83e6ec086a3ab3f3a309b2099db
Jacob Stuart <stuart4@purdue.edu>
2015-06-18 15:04:06 -0700

    adds ability to read and write to driver

--- crandom.c
+++ crandom.c
@@ -4,97 +4,102 @@
 #include <linux/fs.h>
 #include <linux/uaccess.h>
 #include <linux/miscdevice.h>
+#include <linux/slab.h>
 
-#define DEBUG
 #define BUFFER_SIZE 12
+#define DEBUG
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Jacob Stuart");
-MODULE_DESCRIPTION("Hello World");
+MODULE_DESCRIPTION("Generate Pseudo Random Characters");
 
-static const char *id_number = "e64c403f6ead";
+static void
+crandom_err(char *msg)
+{
+	pr_err("crandom: %s\n", msg);
+}
+
+static void
+crandom_base64(unsigned char* byte_to_encode)
+{
+	*byte_to_encode = *byte_to_encode;
+}
 
 static ssize_t
-sample_char_read(struct file *filp, char *buffer,
+crandom_read(struct file *filp, char *buffer,
 		size_t length, loff_t *offset)
 {
-	if (length < sizeof(id_number))
-		return -EINVAL;
+	int i;
+	char *gen_buf = (char *) kmalloc(length, GFP_KERNEL);
 
+	for (i = 0; i < length; i++) {
+		gen_buf[i] = 'a' + i;
+		crandom_base64(&gen_buf[i]);
+	}
 
 	if (*offset != 0)
 		return 0;
 
-	if (copy_to_user(buffer, id_number, BUFFER_SIZE))
+	if (copy_to_user(buffer, gen_buf, length))
 		return -EINVAL;
+
 	*offset = length;
 	return length;
 }
 
 static ssize_t
-sample_char_write(struct file *filp, const char *buffer,
+crandom_write(struct file *filp, const char *buffer,
 		size_t length, loff_t *offset)
 {
-	if (strncmp(buffer, id_number, length) == 0) {
-		pr_err("HELLOWORLD: Success!");
-		return length;
-	}
-
-	pr_err("HELLOWORLD: Wrong ID!");
-	return -EINVAL;
+	return length;
 }
 
 int
-sample_char_open(struct inode *inode, struct file *file)
+crandom_open(struct inode *inode, struct file *file)
 {
-	pr_err("HELLOWORLD: Opened\n");
 	return 0;
 }
 
 int
-sample_char_release(struct inode *inode, struct file *file)
+crandom_release(struct inode *inode, struct file *file)
 {
-	pr_err("HELLOWORLD: Release\n");
 	return 0;
 }
 
 static const struct file_operations fops = {
-	.read = sample_char_read,
-	.write = sample_char_write,
-	.open = sample_char_open,
-	.release = sample_char_release
+	.read = crandom_read,
+	.write = crandom_write,
+	.open = crandom_open,
+	.release = crandom_release
 };
 
 static struct miscdevice hello_device = {
 	.minor = MISC_DYNAMIC_MINOR,
-	.name = "eudyptula",
+	.name = "crandom",
 	.fops = &fops,
 	.mode = S_IRWXUGO
 };
 
 static int __init
-hello_init(void)
+crandom_init(void)
 {
 	int ret = 0;
 
-	pr_err("HELLOWORLD: Initiated\n");
-
 	ret = misc_register(&hello_device);
+
 	if (ret)
-		pr_err("HELLOWORLD: Unable to Register\n");
-	else
-		pr_err("HELLOWORLD: Registered as %d\n", hello_device.minor);
+		crandom_err("Unable to Register\n");
 
 
 	return ret;
 }
 
 static void __exit
-hello_exit(void)
+crandom_exit(void)
 {
 	misc_deregister(&hello_device);
 	pr_err("HELLOWORLD: Destroyed\n");
 }
 
-module_init(hello_init);
-module_exit(hello_exit);
+module_init(crandom_init);
+module_exit(crandom_exit);
